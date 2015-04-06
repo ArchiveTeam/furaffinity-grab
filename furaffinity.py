ac3f214750c6ab27793329fa0c76bafee484e82b
Christopher Foo <chris.foo@gmail.com>
2015-04-06 19:50:13 -0400

    Tweak full url match to be more specific

--- furaffinity.py
+++ furaffinity.py
@@ -62,7 +62,7 @@ def accept_url(url_info, record_info, verdict, reasons):
         if 'facdn.net' in url and 'furaffinity.net/favorites/' in record_info['referrer']:
             return False
 
-    if not verdict and 'facdn.net' in url and '/full/' in record_info['referrer']:
+    if not verdict and 'facdn.net' in url and 'furaffinity.net/full/' in record_info['referrer']:
         return True
 
     return verdict
