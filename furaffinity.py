efe032dd963cf6782c0fb3ea8a4a6f9581cbb340
Christopher Foo <chris.foo@gmail.com>
2015-05-24 23:55:17 -0400

    Lower favs pagination to 500

--- furaffinity.py
+++ furaffinity.py
@@ -50,7 +50,7 @@ def accept_url(url_info, record_info, verdict, reasons):
                     if max_favorites_page is not None and num > max_favorites_page:
                         print_('Pagination complete for favorites')
                         return False
-                    elif num > 1000:
+                    elif num > 500:
                         print_('Wow, this user likes a lot of things! Capped the pagination.')
                         return False
                 else:
