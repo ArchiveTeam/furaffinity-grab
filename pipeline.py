9381e94cd533cd8b1df2a0202e91399be08f951b
Christopher Foo <chris.foo@gmail.com>
2015-06-14 15:48:56 -0400

    Bump version

--- pipeline.py
+++ pipeline.py
@@ -53,7 +53,7 @@ if not WPULL_EXE:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20150527.01"
+VERSION = "20150614.01"
 # USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'furaffinity'
 TRACKER_HOST = 'tracker.archiveteam.org'
